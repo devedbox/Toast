6544e387b21b642a4ddc605dd3ae41ea7c84679c
devedbox <devedbox@qq.com>
2018-06-13 11:13:03 +0800

    Added extension of UIEdgeInsets.

--- Sources/misc.swift
+++ Sources/misc.swift
@@ -6,3 +6,19 @@
 //  Copyright © 2018年 AxziplinLib. All rights reserved.
 //
 
+import UIKit
+
+// MARK: - UIEdgeInsets.
+
+extension UIEdgeInsets {
+    
+    /// Returns the horizontal length of the edge insets.
+    public var width: CGFloat {
+        return left + right
+    }
+    
+    /// Returns the vertical length of the edge insets.
+    public var height: CGFloat {
+        return top + bottom
+    }
+}
