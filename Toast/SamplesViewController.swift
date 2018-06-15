6b9fdabdb8021ab4b79a37e7556c499dedde37f2
devedbox <devedbox@qq.com>
2018-06-15 14:34:57 +0800

    Updated sample codes.

--- Toast/SamplesViewController.swift
+++ Toast/SamplesViewController.swift
@@ -12,9 +12,17 @@ class SamplesViewController: UITableViewController {
     
     let tintColor = UIColor.white
     
+    let pieProgressToast = ToastController.progress(.pie, message: "加载中...")
+    let ringProgressToast = ToastController.progress(.ring, message: "加载中...")
+    let barProgressToast = ToastController.progress(.horizontalBar, message: "加载中...")
+    let colouredProgressToast = ToastController.progress(.colouredBar, message: "加载中...")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.tintColor = .white
+        
+        let timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(_handleTimer(_:)), userInfo: nil, repeats: true)
+        RunLoop.main.add(timer, forMode: .commonModes)
     }
 }
 
@@ -87,18 +95,55 @@ extension SamplesViewController {
     }
     
     private func _showPieProgress() {
-        
+        pieProgressToast.toastView.tintColor = tintColor
+        pieProgressToast.show(in: self, animated: true)
     }
     
     private func _showRingProgress() {
-        
+        ringProgressToast.toastView.tintColor = tintColor
+        ringProgressToast.show(in: self, animated: true)
     }
     
     private func _showBarProgress() {
-        
+        barProgressToast.toastView.tintColor = tintColor
+        barProgressToast.show(in: self, animated: true)
     }
     
     private func _showColouredBarProgress() {
+        colouredProgressToast.toastView.tintColor = tintColor
+        colouredProgressToast.show(in: self, animated: true)
+    }
+}
+
+extension SamplesViewController {
+    @objc
+    private func _handleTimer(_ timer: Timer) {
+        if case let progress? = barProgressToast.progress, progress >= 1.0 {
+            barProgressToast.dismiss(animated: true)
+            barProgressToast.progress = 0.01
+        } else {
+            barProgressToast.progress = barProgressToast.progress.map { $0 + 0.01 }
+        }
         
+        if case let progress? = ringProgressToast.progress, progress >= 1.0 {
+            ringProgressToast.dismiss(animated: true)
+            ringProgressToast.progress = 0.01
+        } else {
+            ringProgressToast.progress = ringProgressToast.progress.map { $0 + 0.01 }
+        }
+        
+        if case let progress? = pieProgressToast.progress, progress >= 1.0 {
+            pieProgressToast.dismiss(animated: true)
+            pieProgressToast.progress = 0.01
+        } else {
+            pieProgressToast.progress = pieProgressToast.progress.map { $0 + 0.01 }
+        }
+        
+        if case let progress? = colouredProgressToast.progress, progress >= 1.0 {
+            colouredProgressToast.dismiss(animated: true)
+            colouredProgressToast.progress = 0.01
+        } else {
+            colouredProgressToast.progress = colouredProgressToast.progress.map { $0 + 0.01 }
+        }
     }
 }
